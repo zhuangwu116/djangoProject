f046912f483984d6e386062b59bab7c4e539f954
vca <zhuangwu@bjvca.com>
2019-01-17 19:08:58 +0800

    ?

--- restdemo/app1/views.py
+++ restdemo/app1/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from app1.serializers import UserSerializer, GroupSerializer
 from django.views import View
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.utils.decorators import method_decorator
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -18,10 +21,26 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+class MyBaseView(object):
 
-class StudentsView(View):
     def dispatch(self, request, *args, **kwargs):
+        print('before')
         ret = super(StudentsView, self).dispatch(request, *args, **kwargs)
+        print('after')
         return ret
+
+class StudentsView(MyBaseView, View):
     def get(self, request, *args, **kwargs):
-        self.dispatch(request, *args, **kwargs)
+        return HttpResponse('GET')
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST')
+    def put(self, request, *args, **kwargs):
+        return HttpResponse('PUT')
+    def delete(self, request, *args, **kwargs):
+        return HttpResponse('DELETE')
+
+@method_decorator(csrf_exempt,name='dispatch')
+class Teacher(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StudentsView, self).dispatch(request, *args, **kwargs)
