a7b90aa874ef943b5ba0f890c65d2a6be79889d8
vca <zhuangwu@bjvca.com>
2019-01-23 17:41:48 +0800

    ?

---
+++ restdemo/apps/goods/urls.py
@@ -0,0 +1,38 @@
+"""restdemo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+
+
+
+from restdemo.settings import MEDIA_ROOT
+from django.views.static import serve
+from rest_framework.documentation import include_docs_urls
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+from .views import GoodsListView
+urlpatterns = [
+
+   url(
+      r'^(?P<version>(v1|v2))/goods/$',
+      GoodsListView.as_view(),
+      name='goods'
+   ),
+
+]
