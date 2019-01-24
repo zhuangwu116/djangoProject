f258cb7d345dc508103dc008c78568d2d7651ba3
zhuangwu <562669088@qq.com>
2019-01-24 23:10:56 +0800

    ?

--- restdjangodemo/db_tools/import_category_data.py
+++ restdjangodemo/db_tools/import_category_data.py
@@ -8,6 +8,7 @@ import os
 
 pwd = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(pwd+"../")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restdjangodemo.settings")
 
 import django
