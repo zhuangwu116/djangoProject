529fc9fd4c8eee8a6e365dc534399d77edaef900
vca <zhuangwu@bjvca.com>
2019-02-14 15:58:32 +0800

    ?

--- restdjangodemo/apps/goods/views.py
+++ restdjangodemo/apps/goods/views.py
@@ -18,7 +18,7 @@ from .filters import GoodsFilter
 class GoodsPagination(PageNumberPagination):
     page_size = 12
     page_size_query_param = 'page_size'
-    page_query_param = "p"
+    page_query_param = "page"
     max_page_size = 100
 
 class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
